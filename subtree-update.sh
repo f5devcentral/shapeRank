d4493d1686c239e576d7deb91721c1a426e856e5
Gilad Bracha <gbracha@gmail.com>
2020-11-30 10:50:31 -0800

    Reorganize sources in separate src directory.

--- subtree-update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-
-git subtree pull --prefix primordialsoup https://github.com/newspeaklanguage/primordialsoup extraRevs --squash
-git subtree pull --prefix newspeak https://github.com/newspeaklanguage/newspeak master --squash
