7a821593f89123850f6777a6824abc7d460449ec
Gilad Bracha <gbracha@gmail.com>
2020-12-01 14:43:06 -0800

    Reviseded build script.

--- build.sh
+++ build.sh
@@ -1,34 +1,41 @@
 #!/bin/bash -ex 
 
+# Change this to point at your emscripten installation
 EMSDK=~/software/emsdk
 
 source $EMSDK/emsdk_env.sh
-pushd primordialsoup
+
+# Assume we cloned psoup into the directory above;
+# change this if your psoup repo is elsewhere
+pushd ../primordialsoup
 ./build os=emscripten arch=wasm
+popd
 
-mkdir -p out/web
-cp out/ReleaseEmscriptenWASM/primordialsoup.* out/web
-cp out/snapshots/*.vfuel out/web
-cp newspeak/*.ns out/web
-../copyIDEDeps.sh ../newspeak out/web
-cp ../*.ns out/web
-cp -R ../CodeMirror out/web
+mkdir -p out
+cp ../primordialsoup/out/ReleaseEmscriptenWASM/primordialsoup.* out
+cp ../primordialsoup/out/snapshots/*.vfuel out
 
-pushd out/web
-../ReleaseX64/primordialsoup \
-    ../snapshots/WebCompiler.vfuel \
+# Copy Psoup sources
+cp ../primordialsoup/newspeak/*.ns out
+# Copy required Newspeak sources
+copyIDEDeps.sh ../newspeak out
+cp src/*.ns out
+cp -R ../CodeMirror out
+
+pushd out
+../primordialsoup/out/ReleaseX64/primordialsoup \
+    ../primordialsoup/out/snapshots/WebCompiler.vfuel \
     *.ns \
     *.png \
     CodeMirror/lib/codemirror.js \
     CodeMirror/addon/display/autorefresh.js \
-    RuntimeWithMirrorsForPrimordialSoup HopscotchWebIDE HopscotchWebIDE.vfuel \
     RuntimeWithMirrorsForPrimordialSoup ShapeRankWithIDE ShapeRankWithIDE.vfuel \
     RuntimeWithMirrorsForPrimordialSoup ShapeRankDemo ShapeRankDemo.vfuel \
     RuntimeWithMirrorsForPrimordialSoup ShapeRankDemo2 ShapeRankDemo2.vfuel \
     RuntimeWithMirrorsForPrimordialSoup ShapeRankCLI ShapeRankCLI.vfuel \
     RuntimeWithMirrorsForPrimordialSoup ShapeRankWorkspaceApp ShapeRankWorkspaceApp.vfuel \
-    RuntimeWithMirrorsForPrimordialSoup ShapeRankNotebook ShapeRankNotebook.vfuel \    
+    RuntimeWithMirrorsForPrimordialSoup ShapeRankNotebook ShapeRankNotebook.vfuel   
     
-#    RuntimeWithMirrorsForPrimordialSoup Ampleforth Ampleforth.vfuel
+
 popd
-popd
+
