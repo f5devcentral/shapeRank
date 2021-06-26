0c363332d2f047e9d089e3881cc0d4a913b57a76
mtrofimciuc-f5 <m.trofimciuc@f5.com>
2021-06-26 00:38:18 -0500

    docker start

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,3 +6,11 @@ import { outputDirectory } from "../package.json"
 
 executeStringCommand(`mkdir -p ${outputDirectory}`);
 executeStringCommand(`cp node_modules/primordialsoupnewspeak/*.ns ${outputDirectory}`);
+
+// docker pull emscripten/emsdk
+
+// sudo apt-get update
+// sudo apt-get install g++-multilib scons
+
+// git clone https://github.com/newspeaklanguage/primordialsoup.git
+// git clone https://github.com/f5devcentral/shapeRank.git
