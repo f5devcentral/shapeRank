28a1189a35b7c814734c8b14651090209e4833c8
Gilad Bracha <gbracha@gmail.com>
2021-08-11 10:14:59 -0700

    WIP on invoking compilers.

--- build_demo_lib/build_demo_lib.js
+++ build_demo_lib/build_demo_lib.js
@@ -1,7 +1,27 @@
 var chokidar = require('chokidar')
+var { exec, spawn } = require('child_process');
 
 function getFiles(path, closure) {
     chokidar.watch(path).on('all', (filepath) => closure(filepath));
 }
 
+function concatDeps(deps) {
+    // convert deps into a single string 
+    var result = ''
+    for d in deps {
+      result = result ++ d ++ ' '
+    }
+    return result
+}
+
+function clang(target, deps, flags) {
+    exec('cc -c ' ++ target ++ ' ' ++ concatDeps(deps))
+}
+
+function basicLink(target, deps, flags) {   
+    exec('cc ' ++ flags ++ ' ' ++ target ++ '-Wl '++ concatDeps(deps)) 
+}
+
 exports.getFiles = getFiles
+exports.clang = clang
+exports.basicLink = basicLink
